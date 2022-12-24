8f193457eb621df850db384789cb61fbffa4a529
linoy9877 <117527644+linoy9877@users.noreply.github.com>
2022-12-24 22:55:33 +0200

    Add files via upload

---
+++ Server/Pages/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HomeWork_LinoyDangur.Shared.Models.Courses;
+using HomeWork_LinoyDangur.Server.Pages.HomeWork_LinoyDangur.Shared.Models.Courses;
+
+namespace HomeWork_LinoyDangur.Server.Controllers
+{
+    [Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+    [HttpGet("{str}")]
+    public async Task<IActionResult> Check(string str)
+    {
+        int space = str.IndexOf(" ");
+        // if space was not found
+        if (space == -1)
+        {
+            return BadRequest(str);
+        }
+        else
+        {
+            return Ok(str.Substring(0, space));
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CheckCourse(Course course)
+    {
+        if (course != null)
+        {
+            if (course.Teacher.IndexOf(" ") == -1)
+            {
+                course.Teacher += "no";
+                return Ok();
+            }
+            else
+            {
+                return Ok(course);
+            }
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+}
+}
---
+++ Server/Pages/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork_LinoyDangur.Server.Pages
+
+
+        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_LinoyDangur.Shared.Models.Courses
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public double Credits { get; set; }
+        public bool Exam { get; set; }
+
+    }
+    }
+}
